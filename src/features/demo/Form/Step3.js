05d91cfac46a714cff066000ba1269851bdd4943
jmcheynier <jmcheynier@gmail.com>
2020-10-28 15:56:22 +0100

    DEV-3 Add form steps + pdf viewer

--- src/features/demo/Form/Step3.js
+++ src/features/demo/Form/Step3.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Step from './Step';
 
+import { Document, Page } from 'react-pdf'; 
+import { pdfjs } from 'react-pdf';
+
+import Button from 'react-bootstrap/Button';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 export default function Step3({step}){
-    return <Step step={step}>Step3</Step>
+
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+  
+    function onDocumentLoadSuccess({ numPages }) {
+      setNumPages(numPages);
+    }
+
+    return <Step step={step}>
+        <Document file={`./European-Accident-Statement.pdf`}
+            onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber}
+                renderInteractiveForms={true} />
+        </Document>
+        <Button disabled={pageNumber === 1} 
+            onClick={e => setPageNumber(pageNumber-1)}>
+                Previous
+        </Button>
+        <Button disabled={pageNumber === numPages} 
+            onClick={e => setPageNumber(pageNumber+1)}>
+                Next</Button>
+
+    </Step>
 } 
