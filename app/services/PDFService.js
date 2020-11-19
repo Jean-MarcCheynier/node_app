bd8b4d3e0c86815a7e0508153b6a097e4774d644
Jean-MarCheynier <jmcheynier@gmail.com>
2020-11-19 11:44:26 +0100

    Eslint nazi

--- app/services/PDFService.js
+++ app/services/PDFService.js
@@ -5,46 +5,43 @@ const path = require('path')
 const StatementFlattenService = require('./statementFlattenService')
 const logger = require('../config/winston')
 
-const fillFormWithFlatten = util.promisify(pdfFiller.fillFormWithFlatten.bind(pdfFiller));
+const fillFormWithFlatten = util.promisify(pdfFiller.fillFormWithFlatten.bind(pdfFiller))
 const currentPath = process.cwd()
 
 const sourcePDF = path.join(currentPath, 'public', 'European-Accident-Statement_form.pdf')
 
-
 /**
  * TODO : if file at path is older than file online, download and replace file
- * options (not exhaustive) : check last modified at each generation, check once every X, force replace every X 
+ * options (not exhaustive) : check last modified at each generation, check once every X, force replace every X
  * @param {string} path
  */
-const checkValidity = (path) =>{
-  if (fs.existsSync(path)) {
-    return true
-  } else {
-    return false
-  }
-}
-
+// const checkValidity = (path) => {
+//   if (fs.existsSync(path)) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
 
 /**
- * using the fiels from statement , fill the template pdf and store it 
- * @param {AccidentStatementSchema} statement 
+ * using the fiels from statement , fill the template pdf and store it
+ * @param {AccidentStatementSchema} statement
  */
 async function generate (statement, id) {
   // template = check_validity(sourcePDF)
   if (!fs.existsSync(sourcePDF)) {
     logger.debug(`error or path to source with cwd = ${currentPath}`)
   }
-
   const destinationPDF = path.join(currentPath, 'public', 'tmp', `${id}.pdf`)
   const flattenData = StatementFlattenService.flatten(statement)
   logger.debug(`flatten data =${JSON.stringify(flattenData)}`)
-  await fillFormWithFlatten(sourcePDF, destinationPDF, flattenData, false);
+  await fillFormWithFlatten(sourcePDF, destinationPDF, flattenData, false)
   return destinationPDF
 }
 
 /**
  * fills a statement using the corrections comming from the user
- * @param {*} statement 
+ * @param {*} statement
  */
 const complete = (statement) => {
 
--- app/services/statementFlattenService.js
+++ app/services/statementFlattenService.js
@@ -1,14 +1,14 @@
-const logger = require("../config/winston")
+const logger = require('../config/winston')
 
 const toString = (dict) => {
   logger.debug(dict)
-  for (var key in dict) {
+  for (const key in dict) {
     logger.debug(dict[key])
     dict[key] = dict[key].valueString
   }
 }
 
-const unflatten = (flatStatement) =>{
+const unflatten = (flatStatement) => {
   return null
 }
 /**
@@ -21,7 +21,7 @@ const unflatten = (flatStatement) =>{
   into a series of fields for insertion in the PDF
  * @param {*} statement
  */
-const flatten = (statement) =>{
+const flatten = (statement) => {
   let flatStatement = {}
 
   if (statement.driverA) {
@@ -31,17 +31,17 @@ const flatten = (statement) =>{
     flatStatement = { ...flatStatement, NameDriverA, FirstNameDriverA, birthDateDriverA }
     toString(flatStatement)
   } else {
-    //logger.debug(`empty field DriverA in statement ${statement}`)
+    // logger.debug(`empty field DriverA in statement ${statement}`)
   }
   flatStatement.NameA = statement._id
-  flatStatement.FirstNameA = "statement.driverA.firstname"
+  flatStatement.FirstNameA = 'statement.driverA.firstname'
   // flatStatement.AddressA = statement.driverA.
   // flatStatement.ZipA = statement.driverA.
   // flatStatement.countryA = statement.driverA.nationality //TODO : find a way to turn it into a nationality correctly
-  //flatStatement.ContactA = statement.driverA.phone?statement.driverA.phone:statement.driverA.email
+  // flatStatement.ContactA = statement.driverA.phone?statement.driverA.phone:statement.driverA.email
   // flatStatement.InsuranceNameA
   // flatStatement.InsurancePolicyA
-  //flatStatement.GreenCardA = statement.driverA.greenCard.imageRef // TODO:switch to the real nA
+  // flatStatement.GreenCardA = statement.driverA.greenCard.imageRef // TODO:switch to the real nA
   // flatStatement.InsuranceDateA
   // flatStatement.InsuranceEndDateA
   // flatStatement.InsuranceCompanyA
@@ -49,13 +49,13 @@ const flatten = (statement) =>{
   // flatStatement.countryInsuranceA
   // flatStatement.ContactInsuranceA
 
-  // flatStatement.NameDriverA 
+  // flatStatement.NameDriverA
   // flatStatement.FirstNameDriverA
   // flatStatement.BirthdateDriverA
   // flatStatement.AddressDriverA
   // flatStatement.countryDriverA
   // flatStatement.ContactDriverA
-  //flatStatement.DrivingLicenseDriverA = statement.driverA.drivingLicense.imageRef
+  // flatStatement.DrivingLicenseDriverA = statement.driverA.drivingLicense.imageRef
   // flatStatement.CategoryDriverA
   // flatStatement.ValidityLicenseDriverA
   // flatStatement.VehicleTypeA
@@ -63,16 +63,16 @@ const flatten = (statement) =>{
   // flatStatement.countryVehicleA
   // flatStatement.VehicleIDTrailerA
   // flatStatement.countryTrailerA
-  
-  flatStatement.NameB = "Doe"
-  flatStatement.FirstNameB = "John"
+
+  flatStatement.NameB = 'Doe'
+  flatStatement.FirstNameB = 'John'
   // flatStatement.AddressB = statement.driverB.
   // flatStatement.ZipB = statement.driverB.
   // flatStatement.countryB = statement.driverB.nationality //TODO : find a way to turn it into a nationality correctly
-  //flatStatement.ContactB = statement.driverB.phone?statement.driverB.phone:statement.driverB.email
+  // flatStatement.ContactB = statement.driverB.phone?statement.driverB.phone:statement.driverB.email
   // flatStatement.InsuranceNameB
   // flatStatement.InsurancePolicyB
-  //flatStatement.GreenCardB = statement.driverB.greenCard.imageRef // TODO:switch to the real nb
+  // flatStatement.GreenCardB = statement.driverB.greenCard.imageRef // TODO:switch to the real nb
   // flatStatement.InsuranceDateB
   // flatStatement.InsuranceEndDateB
   // flatStatement.InsuranceCompanyB
@@ -80,13 +80,13 @@ const flatten = (statement) =>{
   // flatStatement.countryInsuranceB
   // flatStatement.ContactInsuranceB
 
-  // flatStatement.NameDriverB 
+  // flatStatement.NameDriverB
   // flatStatement.FirstNameDriverB
   // flatStatement.BirthdateDriverB
   // flatStatement.AddressDriverB
   // flatStatement.countryDriverB
   // flatStatement.ContactDriverB
-  //flatStatement.DrivingLicenseDriverB = statement.driverB.drivingLicense.imageRef
+  // flatStatement.DrivingLicenseDriverB = statement.driverB.drivingLicense.imageRef
   // flatStatement.CategoryDriverB
   // flatStatement.ValidityLicenseDriverB
   // flatStatement.VehicleTypeB
