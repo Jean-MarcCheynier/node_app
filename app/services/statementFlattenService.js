2fdd6299ef2e5e68cd5ea2ce87027ab8e24534d5
Crastes de Paulet <damien.crastesdepaulet@fujitsu.com>
2020-11-20 17:35:27 +0100

    fix empty ref generation

--- app/services/statementFlattenService.js
+++ app/services/statementFlattenService.js
@@ -1,10 +1,11 @@
+const { now } = require('lodash')
 const logger = require('../config/winston')
 
 const toString = (dict) => {
   logger.debug(dict)
   for (const key in dict) {
-    logger.debug(dict[key])
-    dict[key] = dict[key].valueString
+    //TODO switch between valueDate and valueString based on type
+    dict[key] = dict[key] ? dict[key].text : 'TO FILL'
   }
 }
 
@@ -24,17 +25,56 @@ const unflatten = (flatStatement) => {
 const flatten = (statement) => {
   let flatStatement = {}
 
-  if (statement.driverA) {
+  if (statement.driverA.idCard.imageRef) {
     const fields = statement.driverA.idCard.imageRef
     logger.debug(fields)
-    const { name: NameDriverA, givenNames: FirstNameDriverA, birthDate: birthDateDriverA } = fields.classification
-    flatStatement = { ...flatStatement, NameDriverA, FirstNameDriverA, birthDateDriverA }
-    toString(flatStatement)
+    const { name: NameDriverA, givenNames: FirstNameDriverA, birthDate: BirthdateDriverA } = fields.classification
+    flatStatement = { ...flatStatement, NameDriverA, FirstNameDriverA, BirthdateDriverA }
   } else {
-    // logger.debug(`empty field DriverA in statement ${statement}`)
+    flatStatement.NameDriverA = { text: 'Missing ID' }
   }
-  flatStatement.NameA = statement._id
-  flatStatement.FirstNameA = 'statement.driverA.firstname'
+
+  if (statement.driverA.drivingLicense.imageRef) {
+    const fields = statement.driverA.drivingLicense.imageRef
+    logger.debug(fields)
+    const { licenseId: DrivingLicenseDriverA, expiryDate: ValidityLicenseDriverA } = fields.classification
+    flatStatement = { ...flatStatement, DrivingLicenseDriverA, ValidityLicenseDriverA }
+  } else {
+    flatStatement.DrivingLicenseDriverA = { text: 'Missing license' } // structure required as we use it in toString after//TODO fix that
+  }
+
+  if (statement.driverA.greenCard.imageRef) {
+    const fields = statement.driverA.greenCard.imageRef
+    logger.debug(fields)
+    const {
+      fullname: NameDriverA, policy: InsurancePolicyA, validFrom: InsuranceDateA,
+      validUntil: InsuranceEndDateA, vehicleModel: VehicleTypeA, vehicleNumber: VehicleIDA
+    } = fields.classification
+    flatStatement = { ...flatStatement, NameDriverA, InsurancePolicyA, InsuranceDateA, InsuranceEndDateA, VehicleTypeA, VehicleIDA }
+  } else {
+    flatStatement.NameA = { text: 'Missing Greencard' } // structure required as we use it in toString after//TODO fix that
+  }
+
+  if (statement.driverB.idCard.imageRef) {
+    const fields = statement.driverB.idCard.imageRef
+    logger.debug(fields)
+    const { name: NameDriverB, givenNames: FirstNameDriverB, birthDate: BirthdateDriverB } = fields.classification
+    flatStatement = { ...flatStatement, NameDriverB, FirstNameDriverB, BirthdateDriverB }
+  } else {
+    flatStatement.NameDriverB = { text: 'Missing ID' } // structure required as we use it in toString after//TODO fix that
+  }
+
+  if (statement.driverB.drivingLicense.imageRef) {
+    const fields = statement.driverB.drivingLicense.imageRef
+    logger.debug(fields)
+    const { licenseId: DrivingLicenseDriverB, expiryDate: ValidityLicenseDriverB } = fields.classification
+    flatStatement = { ...flatStatement, DrivingLicenseDriverB, ValidityLicenseDriverB }
+  } else {
+    flatStatement.DrivingLicenseDriverB = { text: 'Missing license' } // structure required as we use it in toString after//TODO fix that
+  }
+  toString(flatStatement)
+  flatStatement.AccidentPlace = statement._id
+  flatStatement.AccidentDate = new Date(Date.now())
   // flatStatement.AddressA = statement.driverA.
   // flatStatement.ZipA = statement.driverA.
   // flatStatement.countryA = statement.driverA.nationality //TODO : find a way to turn it into a nationality correctly
@@ -113,6 +153,10 @@ const flatten = (statement) => {
     flatStatement.damage.truck_back_A
     flatStatement.damage.truck_back_B
     flatStatement.damage.truck_front_A */
+
+  for (const key in flatStatement) {
+    logger.debug(`key : ${key} value : ${flatStatement[key]}`)
+  }
   return flatStatement
 }
 
